48be7915ca9321b963c08af000da7d08741d5f47
Juan Lloret <jlloret@twilio.com>
2022-06-13 08:11:06 +0200

    - docker-package executor - dev-env docker image

---
+++ tools/generators/docker-package/index.ts
@@ -0,0 +1,69 @@
+import { exec } from 'child_process';
+
+import {
+  Tree,
+  formatFiles,
+  readProjectConfiguration,
+  generateFiles,
+  joinPathFragments,
+  updateProjectConfiguration,
+  ProjectConfiguration,
+} from '@nrwl/devkit';
+import { npmPackageGenerator } from '@nrwl/workspace/generators';
+
+type Schema = {
+  name: string;
+  from: string;
+  description: string;
+};
+
+export default async function (tree: Tree, schema: Schema) {
+  await npmPackageGenerator(tree, { name: schema.name });
+  const packageRoot = readProjectConfiguration(tree, schema.name).root;
+
+  // Remove unnecessary files
+  tree.delete(`${packageRoot}/index.js`);
+  tree.delete(`${packageRoot}/package.json`);
+
+  // Add project configuration
+  const config: ProjectConfiguration = {
+    name: schema.name,
+    root: packageRoot,
+    targets: {
+      publish: {
+        executor: '@nx-tools/nx-docker:build',
+        options: {
+          push: true,
+          'cache-from': [
+            `type=registry,ref=ghcr.io/juallom/${schema.name}:buildcache`,
+          ],
+          'cache-to': [
+            `type=registry,ref=ghcr.io/juallom/${schema.name}:buildcache,mode=max`,
+          ],
+          metadata: {
+            images: [`ghcr.io/juallom/${schema.name}`],
+            tags: [
+              'type=schedule',
+              'type=semver,pattern={{version}}',
+              'type=semver,pattern={{major}}.{{minor}}',
+              'type=semver,pattern={{major}}',
+              'type=sha',
+            ],
+          },
+        },
+      },
+    },
+  };
+  updateProjectConfiguration(tree, schema.name, config);
+
+  await formatFiles(tree);
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, './files'),
+    packageRoot,
+    schema
+  );
+  return () => {
+    exec(`git add ${packageRoot}/Dockerfile ${packageRoot}/project.json`)
+  };
+}
